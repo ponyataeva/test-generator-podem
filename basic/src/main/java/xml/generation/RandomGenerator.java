0d01db0588cf50e5d3b8f24fe3f6ef5c786c8f91
KATE\Екатерина <ponyataevaea@gmail.com>
2017-11-17 01:16:39 +0400

    fixed path to xsd; added additional implementation

--- basic/src/main/java/xml/generation/RandomGenerator.java
+++ basic/src/main/java/xml/generation/RandomGenerator.java
@@ -1,26 +1,30 @@
 package xml.generation;
 
 import com.stratumsoft.xmlgen.DefaultValues;
+import com.stratumsoft.xmlgen.SchemaTypeXmlGenerator;
 import com.stratumsoft.xmlgen.XmlGenOptions;
+import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaCollection;
 
-import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.namespace.QName;
 import javax.xml.transform.stream.StreamSource;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.net.URL;
 
 public class RandomGenerator {
 
-    public void generate() {
-        String path = "basic/src/test/resources/test_1.xml";
-        InputStream is = this.getClass().getResourceAsStream(path);
-        URL xsdUrl = this.getClass().getResource(path);
+    public void generate() throws FileNotFoundException {
+        String path = "basic/src/main/resources/knowledge_base.xsd";
+        File file = new File(path);
+        InputStream is = new FileInputStream(file);
 
         XmlSchemaCollection coll = new XmlSchemaCollection();
-        coll.setBaseUri(xsdUrl.toString());
+        coll.setBaseUri(file.toString());
 
         StreamSource source = new StreamSource(is);
-        XmlSchema schema = (XmlSchema) coll.read(source);
+        XmlSchema schema = coll.read(source);
 
         XmlGenOptions options = new XmlGenOptions();
         options.setGenCommentsForParticles(true);
@@ -28,7 +32,9 @@ public class RandomGenerator {
         options.setMaxRecursiveDepth(1);
         options.setMaxRepeatingElements(2);
         options.setDefVals(DefaultValues.DEFAULT);
-
+        SchemaTypeXmlGenerator generator = new SchemaTypeXmlGenerator(coll, options);
+        boolean isPretty = true;
+        System.out.println(generator.generateXml(new QName("root"), isPretty));
 
     }
 }
